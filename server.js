791a40f5e30e25251c0c99a88f231125c135f895
ASviridov <amsviridov@simple.ru>
2022-01-10 18:51:02 +0300

    new life

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 import express from "express";
+import { dirname, join } from "path";
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("static/"));
-app.use("/src", express.static("src"));
+app.use(express.static(join(__dirname, "static")));
+app.use("/static", express.static(join(__dirname, "static")));
+app.use("/src", express.static(join(__dirname, "src")));
 
 
-app.listen(PORT, function(request, response) {
+app.listen(PORT, function (request, response) {
     console.log(`Server started at ${PORT}`);
 });
