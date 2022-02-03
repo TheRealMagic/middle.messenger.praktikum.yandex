1598081418ff70d7adad60f9f9be4040924e75a2
ASviridov <amsviridov@simple.ru>
2022-02-03 21:00:02 +0300

    1

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
-/* import express from "express";
-import { dirname, join } from "path";
-import { fileURLToPath } from 'url'; 
-
-const __dirname = dirname(fileURLToPath(import.meta.url));*/
-
 const express = require("express");
 const {join} = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-/*app.use(express.static(join(__dirname, "static")));
-app.use("/static", express.static(join(__dirname, "static")));
-app.use("/src", express.static(join(__dirname, "src")));*/
-
 app.use(express.static(join(__dirname, "dist")));
-
-/*app.use(express.static(join(__dirname, "static")));
-app.use("/static", express.static(join(__dirname, "tscrpt/static")));
-app.use("/src", express.static(join(__dirname, "tscrpt/src")));*/
-
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`);
