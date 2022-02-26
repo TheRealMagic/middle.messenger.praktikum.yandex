9bfc253349cc515cfafd6e61de10f7378c9c4cac
ASviridov <amsviridov@simple.ru>
2022-02-26 19:22:14 +0300

    fix

--- src/controllers/ChatsPageController.ts
+++ src/controllers/ChatsPageController.ts
@@ -1,6 +1,6 @@
 import {BaseController} from "./BaseController";
 import {Chat} from "../modules/chat/chat";
-import {ChatsPage} from "../pages/Chats/chats";
+import {ChatsPage} from "../pages/Chats";
 import {ChatPageApi} from "../utils/API/ChatPageApi";
 import ApplicationStore, {StoreEvents} from "../modules/ApplicationState/ApplicationStore";
 import {Input} from "../components/Input/input";
